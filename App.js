e50dc703250a58228cd793cdfafcb67406529204
Shivam <SINGHSHIVAMKR@GMAIL.COM>
2020-08-06 13:20:32 +0530

    added header

--- App.js
+++ App.js
@@ -11,7 +11,12 @@ import {
   Button,
   Alert,
   ToastAndroid,
+  TouchableOpacity,
+  ActivityIndicator,
+  Dimensions,
 } from 'react-native';
+
+
 import {ExecuteQuery} from './storage';
 
 import TableData from './component/TableData';
@@ -19,6 +24,7 @@ import {Table, Row, Rows, TableWrapper} from 'react-native-table-component';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {getLargestWidths} from './component/utils';
 
+const {height, width} = Dimensions.get('window');
 const App = () => {
   const [value, onChangeText] = React.useState('select * from employees');
 
@@ -28,17 +34,22 @@ const App = () => {
 
   const runQuery = async () => {
     try {
-      ToastAndroid.showWithGravity(
-        'Executing Query',
-        ToastAndroid.SHORT,
-        ToastAndroid.BOTTOM,
-      );
       const res = await ExecuteQuery(value);
-
-      const header = Object.keys(res.rows.item(0)).reverse();
 
       const len = res.rows.length;
       const resultArr = [];
+
+      if (len < 1) {
+        console.log('nothing found');
+        ToastAndroid.showWithGravity(
+          'Query Executed',
+          ToastAndroid.SHORT,
+          ToastAndroid.BOTTOM,
+        );
+        return;
+      }
+
+      const header = Object.keys(res.rows.item(0)).reverse();
 
       for (let i = 0; i < len; i++) {
         let row = res.rows.item(i);
@@ -46,7 +57,7 @@ const App = () => {
       }
       // pass the header and result arr to get the largest widths of their respective column
       tableWidths.current = await getLargestWidths([header, ...resultArr]);
-      // console.log(tableWidths);
+      console.log(tableWidths);
 
       // console.log(resultArr);
       setDbData(resultArr);
@@ -59,55 +70,54 @@ const App = () => {
 
   return (
     <>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" />
       <SafeAreaView>
-        <View style={styles.scrollView}>
-          <View>
+        <View style={styles.outerContainer}>
+          <View style={styles.appBar}>
+            <Icon name="menu" size={24} />
+            <Text style={styles.appBarTxt}>SQL Playground</Text>
+          </View>
+          <View style={styles.innercontainer}>
+            <Text style={styles.inputHeader}>Type your SQL Query</Text>
             <TextInput
-              style={{
-                borderColor: 'gray',
-                borderWidth: 1,
-                fontFamily: 'monospace',
-              }}
+              style={styles.input}
               onChangeText={(text) => onChangeText(text)}
               multiline
+              textAlignVertical="top"
               value={value}
               autoCorrect={false}
               numberOfLines={4}
               placeholder="Type your SQL query"
             />
-          </View>
 
-          <Button title="Clear" onPress={() => onChangeText('')} />
-          <Text>Output</Text>
-          <View style={styles.outPutContainer}>
-            <ScrollView horizontal={true} bounces={false}>
-              <View>
-                <ScrollView bounces={false}>
-                  <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
-                    <Row
-                      data={dbHeader}
-                      style={styles.head}
-                      textStyle={styles.headerText}
-                      widthArr={tableWidths.current}
-                    />
-                    <Rows
-                      data={dbData}
-                      widthArr={tableWidths.current}
-                      textStyle={styles.rowTxt}
-                    />
-                  </Table>
-                </ScrollView>
-              </View>
-            </ScrollView>
+            <Button title="Clear" onPress={() => onChangeText('')} />
+            <Text>Output</Text>
+            <View style={styles.outPutContainer}>
+              <ScrollView horizontal={true} bounces={false}>
+                <View>
+                  <ScrollView bounces={false}>
+                    <Table
+                      borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+                      <Row
+                        data={dbHeader}
+                        style={styles.head}
+                        textStyle={styles.headerText}
+                        widthArr={tableWidths.current}
+                      />
+                      <Rows
+                        data={dbData}
+                        widthArr={tableWidths.current}
+                        textStyle={styles.rowTxt}
+                      />
+                    </Table>
+                  </ScrollView>
+                </View>
+              </ScrollView>
+            </View>
           </View>
-
-          <Pressable
-            onPress={runQuery}
-            android_ripple={true}
-            style={styles.runBtn}>
-            <Icon name="play-circle-filled" size={50} color="#2ecc71" />
-          </Pressable>
+          <TouchableOpacity onPress={runQuery} style={styles.runBtn}>
+            <Icon name="play-arrow" size={45} color="#fff" />
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     </>
@@ -115,21 +125,48 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
+  outerContainer: {
     height: '100%',
     width: '100%',
-    padding: 10,
+    backgroundColor: '#fff',
+  },
+  appBar: {
+    width: '100%',
+    height: 45,
+    backgroundColor: 'gold',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  appBarTxt: {
+    textAlign: 'center',
+    fontSize: 22,
+  },
+  innercontainer: {
+    padding: 5,
+  },
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    fontSize: 16,
+    fontFamily: 'monospace',
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  inputHeader: {
+    fontSize: 16,
   },
   runBtn: {
     position: 'absolute',
     bottom: 10,
     right: 10,
+    padding: 5,
+    backgroundColor: '#2ecc71',
+    borderRadius: 150,
   },
 
   outPutContainer: {
     flex: 1,
-    padding: 5,
-    backgroundColor: '#fff',
     marginBottom: 50,
   },
 
