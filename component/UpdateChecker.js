87e7a73d08b9eba2dbdc1fc2b65262df698617e3
Shivam <SINGHSHIVAMKR@GMAIL.COM>
2020-08-08 20:20:02 +0530

    added whatsa new

--- component/UpdateChecker.js
+++ component/UpdateChecker.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import pkginfo from '../package.json'
+import React, {useEffect} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import pkginfo from '../package.json';
+
+const appInfoURL =
+  'https://raw.githubusercontent.com/ShivamJoker/SQL-Playground/master/package.json';
 
 export default function UpdateChecker() {
-    return (
-        <View>
-            <Text>{pkginfo.version}</Text>
-        </View>
-    )
+  const fetchAppDetails = async () => {
+    const res = await fetch(appInfoURL);
+    const json = await res.json();
+    console.log(json);
+  };
+  
+  useEffect(() => {
+    fetchAppDetails();
+  }, []);
+
+  return (
+    <View>
+      <Text>{pkginfo.version}</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
