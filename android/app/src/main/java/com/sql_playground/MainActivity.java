20bd1fc956ecbc31836aa18175c2edeb8bb27941
Shivam <singhshivamkr@gmail.com>
2020-10-21 23:16:46 +0530

    fixed splash

--- android/app/src/main/java/com/sql_playground/MainActivity.java
+++ android/app/src/main/java/com/sql_playground/MainActivity.java
@@ -1,23 +1,34 @@
 package com.sql_playground;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
-import android.content.Intent; 
+
+import android.content.Intent;
 import android.content.res.Configuration;
+
+import org.devio.rn.splashscreen.SplashScreen;
+
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "SQL_PlayGround";
-  }
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "SQL_PlayGround";
+    }
 
-  @Override
-  public void onConfigurationChanged(Configuration newConfig) {
-    super.onConfigurationChanged(newConfig);
-    getReactInstanceManager().onConfigurationChanged(this, newConfig);
-  }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        getReactInstanceManager().onConfigurationChanged(this, newConfig);
+    }
 }
 
