1b4784eb62909038abeff4997993c2457ad39e45
Shivam <singhshivamkr@gmail.com>
2022-04-18 23:45:03 +0530

    :bug: Upgrade to use new AppDelegate.mm

--- ios/SQL_PlayGround/AppDelegate.mm
+++ ios/SQL_PlayGround/AppDelegate.mm
@@ -41,7 +41,7 @@
   bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
 #endif
 
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"RnDiffApp", nil);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"SQL_PlayGround", nil);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
