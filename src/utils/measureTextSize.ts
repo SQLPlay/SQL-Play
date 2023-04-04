7622413a83e86e598e0574419643c523caecaf54
Shivam <singhshivamkr@gmail.com>
2023-04-04 22:01:00 +0530

    :sparkles: Add MeasureSizeModule to calculate text widths

---
+++ src/utils/measureTextSize.ts
@@ -0,0 +1,34 @@
+import {NativeModules} from 'react-native';
+import {memoizeWithLimit} from 'sonic-memoize';
+const {MeasureSizeModule} = NativeModules;
+
+export const measureTextSize = (text: string, font = 'Roboto', size = 16) => {
+  return MeasureSizeModule.measureSize(text, font, size) as Promise<number>;
+};
+
+const _calculateWidths = async (header: string[], rows?: string[][]) => {
+  if (!rows || !rows.length) {
+    throw Error('Empty rows received');
+  }
+
+  const headerWidths: number[] = [];
+  for (let i = 0; i < header.length; i++) {
+    let longestText = '';
+    let maxWidth = 0;
+    for (let j = 0; j < rows.length; j++) {
+      const text = `${rows[j][i]}`;
+      if (text && text.length > longestText.length) {
+        longestText = text;
+      }
+    }
+    const charWidth = await measureTextSize(longestText);
+    const headerCharWidth = await measureTextSize(header[i]);
+    maxWidth = Math.max(charWidth, headerCharWidth);
+    maxWidth = Math.min(maxWidth, 200);
+    headerWidths.push(maxWidth + 36);
+  }
+
+  return headerWidths;
+};
+
+export const calculateWidths = memoizeWithLimit(_calculateWidths, 20);
