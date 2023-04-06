ae92c7f2fa7d1bb474128640501af3556d669bb0
Shivam <singhshivamkr@gmail.com>
2023-04-06 07:24:33 +0530

    :wrench: configure eslint for jest and detox

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
+  env: {
+    // ... other environments
+    'jest/globals': true,
+  },
+  globals: {
+    // ... other globals
+    by: 'readonly',
+    device: 'readonly',
+    element: 'readonly',
+    expect: 'readonly',
+    waitFor: 'readonly',
+  },
   extends: '@react-native-community',
+  plugins: ['jest'],
 };
