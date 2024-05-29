08b06c5c97f473e7016f27566d4b1d6fd69add33
Shivam <singhshivamkr@gmail.com>
2024-05-29 19:43:55 +0530

    add firebase messaging

--- index.js
+++ index.js
@@ -4,9 +4,12 @@
 
 import {AppRegistry} from 'react-native';
 // import 'react-native-gesture-handler';
-import App from './src/App';
 import {name as appName} from './app.json';
-// import NoApp from './NoApp';
+import messaging from '@react-native-firebase/messaging';
+import App from './src/App';
 
-// console.log('yo bro');
 AppRegistry.registerComponent(appName, () => App);
+
+messaging().setBackgroundMessageHandler(
+  () => new Promise(resolve => resolve()),
+);
